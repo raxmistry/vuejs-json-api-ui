1ea07f7d19ade38c599920258a190ddfa896990e
Rakesh Mistry <rakesh.mistry@gmail.com>
2019-05-20 22:08:30 +0200

    Added button that can show a hidden div

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,14 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+      <div class="mybutton" v-on:click="showdiv">
+          <p>My Button</p>
+      </div>
+      <div id="hideme" hidden="true">
+          <p>This should be hidden</p>
+      </div>
+  </div>
   </div>
 </template>
 
@@ -35,6 +17,13 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  methods: {
+      showdiv: function(event) {
+          console.log("showdiv event");
+          var element = document.getElementById("hideme");
+          element.hidden = false;
+      }
   }
 }
 </script>
@@ -54,5 +43,27 @@ li {
 }
 a {
   color: #42b983;
+}
+.mybutton {
+  color: blue;
+  margin: 5px 5px 5px 5px;
+  padding: 10px;
+  border-style: solid;
+  border-width: 2px ;
+  border-radius: 25px;
+  width: 50px;
+  box-shadow: 0 9px #999;
+  text-align: center;
+  
+}
+
+.mybutton:hover {
+  background-color: lightblue;
+}
+
+.mybutton:active {
+  background-color: lightblue;
+  box-shadow: 0 5px #666;
+  transform: translateY(4px);
 }
 </style>
